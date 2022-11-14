c811b847d7062aea06e959bcf83058cb62a84497
Alaysia Veal <alaysiaveal@gmail.com>
2022-11-14 14:02:10 -0500

    added small change in Login page

---
+++ client/src/Pages/Login.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login to submit review</h1>
+    </div>
+  )
+}
