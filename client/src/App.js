6aeab2a406ed63aead9b48a8f7ef9b2caab29af6
aizealawin <ktmudalige@gmail.com>
2022-11-14 10:08:40 -0500

    fixed routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
 import { Route, Routes } from 'react-router-dom'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Restaurant from './Pages/Restaurant'
+import Register from './Pages/Register'
+import Login from './Pages/Login'
+import Nav from './Components/Nav'
 import './Styles/App.css'
 
-
 function App() {
-  return <div className="App">
-    <header className='nav'>
-      <Nav  />
-    </header>
-    <main>
-      <Routes>
-        <Route path = "/" element={ <Home /> }/>
-        <Route path = "/:restaurantId" element={ <Restaurant/> } />
-        <Route path = "/register" element = { <Register/> } />
-        <Route path = "/login" element = {<Login />} />
-        
-      </Routes>
-    </main>
-  </div>
+  return (
+    <div className="App">
+      <header className="nav">
+        <Nav />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:restaurantId" element={<Restaurant />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
