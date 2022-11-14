0571d97645b9037b3bf3cc96b170370ac745cf5c
MohamedOsman15 <osman.mohamed2244@gmail.com>
2022-11-14 08:48:03 -0600

    Set up Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
-import logo from './logo.svg'
+import { Route, Routes } from 'react-router-dom'
+
 import './Styles/App.css'
 
+
 function App() {
-  return <div className="App"></div>
+  return <div className="App">
+    <header className='nav'>
+      <Nav  />
+    </header>
+    <main>
+      <Routes>
+        <Route path = "/" element={ <Home /> }/>
+        <Route path = "/:restaurantId" element={ <Restaurant/> } />
+        <Route path = "/register" element = { <Register/> } />
+        <Route path = "/login" element = {<Login />} />
+        
+      </Routes>
+    </main>
+  </div>
 }
 
 export default App
