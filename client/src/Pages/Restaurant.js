1cb047a66afa43de84db9f6f71e89aa02349b306
MohamedOsman15 <osman.mohamed2244@gmail.com>
2022-11-15 09:46:09 -0600

    new review input form

--- client/src/Pages/Restaurant.js
+++ client/src/Pages/Restaurant.js
@@ -28,6 +28,7 @@ const Restaurant = () => {
   }, [])
 
   const [users, updateUsers] = useState([])
+
   useEffect(() => {
     const api = async () => {
       let res = await axios.get(`http://localhost:3001/api/user`)
@@ -36,12 +37,36 @@ const Restaurant = () => {
     api()
   }, [])
 
+  const initialValues = {
+    restaurantId: restaurantId,
+    userId: '',
+    content: '',
+    rating: '',
+    
+  }
+
+  const [formValues, setFormValues] = useState(initialValues)
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await axios.post(`http://localhost:3001/api/review`, {
+      restaurantId: formValues.restaurantId,
+      userId: formValues.userId,
+      content: formValues.content,
+      rating: formValues.rating
+    })
+  }
+
   return (
     <div>
       {restaurants.map((res) => {
         return (
           <div className="image" key={res.id}>
-            <img src={res.image} />
+            <img src={res.image} alt={res.name}/>
             <h1>{res.name}</h1>
             <div className="menu">
               <h3>Menu</h3>
@@ -49,13 +74,39 @@ const Restaurant = () => {
             </div>
             <div className="reviews">
               <h3>Reviews</h3>
+              <div className='addReview'>
+                <h4>Add Review:</h4>
+                <input
+                type="text"
+                name='content'
+                onChange={handleChange}
+                value={formValues.content}
+                placeholder="Review Comments"
+                required
+                >
+                </input>
+                <select 
+                name='rating'
+                onChange={handleChange}
+                value={formValues.rating}
+                defaultValue="default"
+                >
+                <option value="default" hidden>Rating</option>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+                </select>
+                <button onClick={handleSubmit}>Submit</button>
+              </div>
               {reviews.map((res) => {
                 for (let i = 0; i < users.length; i++) {
                   if (users[i].id === res.userId) {
                     return (
                       <div className="userReview" key={res.id}>
                         <h5>{users[i].username}</h5>
-                        <img src={users[i].profilePic} className="pfp" />
+                        <img src={users[i].profilePic} className="pfp" alt={users[i].username}/>
                         <p>{res.content}</p>
                       </div>
                     )
