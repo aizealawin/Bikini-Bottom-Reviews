93546b9093a29519d225bff0450e469a85d20c5a
aizealawin <ktmudalige@gmail.com>
2022-11-15 09:09:57 -0500

    bug fix

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -18,41 +18,41 @@ const Login = ({ setUser, toggleAuthenticated }) => {
   }
   return (
     <body className="login">
-    <div className="login-form">
-      <h1>Login to submit review</h1>
+      <div className="login-form">
+        <h1>Login to submit review</h1>
 
-      <div className="form">
-        <form onSubmit={handleSubmit}>
+        <div className="form">
+          <form onSubmit={handleSubmit} />
 
-      <div className="submit-form">
-        <form>
-
-          <div className="input-container">
-            <label>Username </label>
-            <input
-              type="text"
-              onChange={handleChange}
-              name="username"
-              required
-            />
-            {/* {renderErrorMessage("uname")} */}
+          <div className="submit-form">
+            <form>
+              <div className="input-container">
+                <label>Username </label>
+                <input
+                  type="text"
+                  onChange={handleChange}
+                  name="username"
+                  required
+                />
+                {/* {renderErrorMessage("uname")} */}
+              </div>
+              <div className="input-container">
+                <label>Password </label>
+                <input
+                  type="password"
+                  onChange={handleChange}
+                  name="password"
+                  required
+                />
+                {/* {renderErrorMessage("pass")} */}
+              </div>
+              <div className="button-container">
+                <input type="submit" />
+              </div>
+            </form>
           </div>
-          <div className="input-container">
-            <label>Password </label>
-            <input
-              type="password"
-              onChange={handleChange}
-              name="password"
-              required
-            />
-            {/* {renderErrorMessage("pass")} */}
-          </div>
-          <div className="button-container">
-            <input type="submit" />
-          </div>
-        </form>
+        </div>
       </div>
-    </div>
     </body>
   )
 }
