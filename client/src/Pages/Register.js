c3fa41ef8e7aac0ab38e089cc34606ebd58d7b09
Thomalita Burbank <thomalita37@gmail.com>
2022-11-16 09:18:12 -0700

    register css

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -29,12 +29,13 @@ const Register = () => {
     navigate('/login')
   }
   return (
-    <div className="form">
+    <div className="register-form">
       <h1>Register to create account</h1>
       <div className="form">
         <form className="register" onSubmit={handleSubmit}>
           <div className="input-container">
-            <label>Email</label>
+            <ul>
+            <li><label>Email: </label>
             <input
               type="text"
               name="email"
@@ -42,8 +43,8 @@ const Register = () => {
               value={formValues.email}
               placeholder="email"
               required
-            ></input>
-            <label>Username </label>
+            ></input></li>
+            <li><label>Username: </label>
             <input
               type="text"
               onChange={handleChange}
@@ -51,20 +52,21 @@ const Register = () => {
               placeholder="username"
               name="username"
               required
-            />
-            <label>Picture </label>
+            /></li>
+            <li><label>Picture: </label>
             <input
               type="text"
               onChange={handleChange}
               value={formValues.profilePic}
-              placeholder="image"
+              placeholder="image-url"
               name="profilePic"
               required
-            />
+            /></li></ul>
             {/* {renderErrorMessage("uname")} */}
           </div>
           <div className="input-container">
-            <label>Password </label>
+            <ul>
+            <li><label>Password: </label>
             <input
               type="password"
               onChange={handleChange}
@@ -72,8 +74,8 @@ const Register = () => {
               placeholder="password"
               name="password"
               required
-            />
-            <label> Confirm Password </label>
+            /></li>
+            <li><label>Confirm Password: </label>
             <input
               type="password"
               onChange={handleChange}
@@ -81,14 +83,16 @@ const Register = () => {
               placeholder="password"
               name="password"
               required
-            />
+            /></li></ul>
             {/* {renderErrorMessage("pass")} */}
           </div>
           <div className="button-container">
+            <ul>
             <input
               type="submit"
               disabled={!formValues.email || !formValues.password}
             />
+            </ul>
           </div>
         </form>
       </div>
